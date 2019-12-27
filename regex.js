048740064bb4eed210c2f57915614d3b8f695d50
Nathan Persall <npersall@gmail.com>
2019-12-27 13:25:58 -0800

    inital commit

---
+++ regex.js
@@ -0,0 +1,19 @@
+const matchOne = (pattern, text) => {
+
+}
+
+
+const search = (pattern, text) => {
+
+}
+
+
+const match = (pattern, text) => {
+
+}
+module.exports = {
+
+  matchOne,
+  match,
+  search
+}
